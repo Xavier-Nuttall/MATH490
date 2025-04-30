6420bdcf558c94f7e978303ac888ae08dc9a3b94
nutxa216 <nutxa216@student.otago.ac.nz>
2025-04-30 20:30:57 +1200

    removed prints from template

--- verticalBarrierSingleTruncation.m
+++ verticalBarrierSingleTruncation.m
@@ -16,7 +16,7 @@ gravity = 9.81;
 period = 5;
 frequency = 2*pi/period;
 alpha = frequency^2/gravity;
-%%
+%% Wave numbers
 
 waveNumbers = dispersion_free_surface(alpha, truncation-1, maxDepth) * 1i;
 waveNumbers(1) = -waveNumbers(1);
@@ -30,36 +30,13 @@ weights = eye(colocationPointCount) * deltaZ;
 weights(1,1) = weights(1,1)/2;
 weights(end,end) = weights(end,end)/2;
 
-
-
-%% For testing the Kernel function
-% kernelSlow should be the same as kernel
-% tic
-% kernelSlow = zeros(colocationPointCount);
-% for i = 1:colocationPointCount
-%     for j = 1:colocationPointCount
-%         for k = 1:truncation
-%             kernelSlow(j,i) = kernelSlow(j,i) + ...
-%                 phi(colocationPoints(i), waveNumbers(k), maxDepth) ...
-%                 * phi(colocationPoints(j), waveNumbers(k), maxDepth) ...
-%                 / (phi_norm_square(waveNumbers(k), maxDepth, barrierDepth) * 1i * waveNumbers(k));
-%         end
-%     end
-% end
-% toc
-%%
-
 %% Calculating u and kernel
-tic
 kernel = getKernel(waveNumbers, colocationPoints, maxDepth, barrierDepth);
-toc
 
 
 functionValue = f(A(1), colocationPoints, waveNumbers(1), maxDepth);
 
 u = (kernel * weights) \ functionValue;
-
-%%
 
 %% Calculate qcoefficients for B and C
 
@@ -68,18 +45,6 @@ B = A - diag(1./(1i * reducedWaveNumbers .* phi_norm_square(reducedWaveNumbers, 
 
 % Calculate coefficients for phi+
 C = D + diag(1./(1i * reducedWaveNumbers .* phi_norm_square(reducedWaveNumbers, maxDepth, barrierDepth))) * phi(colocationPoints, reducedWaveNumbers, maxDepth).' * weights * u;
-%%
-
-%% Printouts
-fprintf("--------------------------------\n")
-fprintf("Truncaion %d:\n\n", truncation)
-fprintf("Energy Conservation for truncation %d: |%d|^2 + |%d|^2 - |%d|^2 =  %d\n",truncation, B(1), C(1), A(1), abs(B(1)^2) + abs(C(1)^2) - abs(A(1))^2)
-
-fprintf("Difference phi+ and phi- along x=0 with truncation %d: %d\n", truncation, sum(A) - sum(B) - sum(C))
-
-values = zeros(truncation);
-bZeros = zeros(truncation);
-cZeros = zeros(truncation);
 
 %% Functions
 function output = phi_norm_square(waveNumbers, maxDepth, barrierDepth) %#ok<INUSD>
@@ -104,5 +69,3 @@ end
 function output = f(A, z, waveNumbers, maxDepth)
     output = A * phi(z, waveNumbers, maxDepth);
 end
-
-%%
