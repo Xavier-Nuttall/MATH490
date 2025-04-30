003a37b906429e162d4b7023b9d4d32ee30e328e
nutxa216 <nutxa216@student.otago.ac.nz>
2025-04-30 20:43:12 +1200

    created template function

--- verticalBarrierSingleTruncation.m
+++ verticalBarrierSingleTruncation.m
@@ -1,19 +1,27 @@
 %% Parameters
-truncation = 1000;
-modeTruncation = 10;
-
-colocationPointCount = 200;
-maxDepth = 20;
-barrierDepth = 5;
-
+function [A,B,C,D] = verticalBarrierSingleTruncation(maxDepth, barrierDepth, period, colocationPointCount, truncation, modeTruncation, positiveIncidentAmplitude, negativeIncidentAmplitude)
+if nargin < 9
+    negativeIncidentAmplitude = 0;
+end
+if nargin < 8
+    positiveIncidentAmplitude = 1;
+end
+if nargin < 7
+    modeTruncation = 10;
+end
+if nargin < 6
+    truncation = 1000;
+end
+if nargin < 5
+    colocationPointCount = 50;
+end
 
 A = zeros(1, modeTruncation).';
 D = zeros(1, modeTruncation).';
-A(1) = 1;
-
+A(1) = positiveIncidentAmplitude;
+D(1) = negativeIncidentAmplitude;
 
 gravity = 9.81;
-period = 5;
 frequency = 2*pi/period;
 alpha = frequency^2/gravity;
 %% Wave numbers
@@ -41,31 +49,33 @@ u = (kernel * weights) \ functionValue;
 %% Calculate qcoefficients for B and C
 
 % Calculate coefficents for phi-
-B = A - diag(1./(1i * reducedWaveNumbers .* phi_norm_square(reducedWaveNumbers, maxDepth, barrierDepth))) * phi(colocationPoints, reducedWaveNumbers, maxDepth).' * weights * u; 
+B = A - diag(1./(1i * reducedWaveNumbers .* phi_norm_square(reducedWaveNumbers, maxDepth, barrierDepth))) * phi(colocationPoints, reducedWaveNumbers, maxDepth).' * weights * u;
 
 % Calculate coefficients for phi+
 C = D + diag(1./(1i * reducedWaveNumbers .* phi_norm_square(reducedWaveNumbers, maxDepth, barrierDepth))) * phi(colocationPoints, reducedWaveNumbers, maxDepth).' * weights * u;
 
+end
 %% Functions
 function output = phi_norm_square(waveNumbers, maxDepth, barrierDepth) %#ok<INUSD>
 
-    output = (...
-        maxDepth + sinh(2 * waveNumbers * maxDepth) ...
-            ./ (2 * waveNumbers)... 
-        ) /2;
+output = (...
+    maxDepth + sinh(2 * waveNumbers * maxDepth) ...
+    ./ (2 * waveNumbers)...
+    ) /2;
 end
 
 function output = getKernel(waveNumbers, colocationPoints, maxDepth, barrierDepth)
-    Phi = phi(colocationPoints, waveNumbers, maxDepth);
-    K = diag(1./(phi_norm_square(waveNumbers, maxDepth, barrierDepth) .* 1i .* waveNumbers));
+Phi = phi(colocationPoints, waveNumbers, maxDepth);
+K = diag(1./(phi_norm_square(waveNumbers, maxDepth, barrierDepth) .* 1i .* waveNumbers));
 
-    output = Phi * K * Phi.';
+output = Phi * K * Phi.';
 end
 
 function output = phi(z,waveNumbers, maxDepth)
-    output = cosh((z + maxDepth) * waveNumbers);
+output = cosh((z + maxDepth) * waveNumbers);
 end
 
 function output = f(A, z, waveNumbers, maxDepth)
-    output = A * phi(z, waveNumbers, maxDepth);
+output = A * phi(z, waveNumbers, maxDepth);
 end
+
